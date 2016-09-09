61685189576f94514910d126dfe2a1e522cceb79
Justin K Smith <js456@pvhs-lab112-d09.pvsd.org>
2016-09-09 09:54:23 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String s; //Declaring
+        s = "Holla!"; //Initializing
+        String another = "Hey Homie"; // all on the same line
+        System.out.println("Holla!");
+        String s2 = "";
+        //later on...
+        s2 = "Nirali";
+
+        int age = 89;
+        //cant just call the variable whatever i want, there are some rules
+
+        //1. no names that are reserved
+        //such as for, if, do, while, system, else, etc.
+        //2. cannot use the same named variable
+        //3. cant use numbers, but i can use "a2" **no starting with numbers, can put them on the end though"
+        //4. symbols
+        //5. NO SPACES! **use underscore or capitlaize the next word
+        //6. Justin_Smith or JustinSmith or CapitalizeTheFirstLetterAsYouGo
+
+        double b = 34.048465132154865131548;
+
+        // 3 new data types -- int, double, and string
+        // int and double ar primitive data types
+        // String is a more advanced data type
+
+    }
+}
